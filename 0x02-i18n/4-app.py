c08f0b78bb3c37e2492e4a584e74cfa7def8be72
Maddie <115709272+Maddily@users.noreply.github.com>
2024-07-30 15:09:55 +0300

    Accept locale argument in URL

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+This module contains a Flask application that supports
+internationalization (i18n).
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """
+    Configuration class for the Flask application.
+    """
+
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get the best matching language locale based on the user's preferences.
+
+    Returns:
+        str: The best matching language locale.
+    """
+
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def welcome():
+    """
+    Renders the welcome page.
+
+    Returns:
+        The rendered HTML template for the welcome page.
+    """
+
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

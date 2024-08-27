f2039e024156652fba5bedcbcb599787e718a058
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-27 10:02:38 +0300

    Create a queueing system

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+redisClient.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+redisClient.subscribe('holberton school channel');
+redisClient.connect();
+
+redisClient.on('message', (channel, message) => {
+  console.log(message);
+
+  if (message === 'KILL_SERVER') {
+    redisClient.unsubscribe(channel);
+    redisClient.quit();
+  }
+});

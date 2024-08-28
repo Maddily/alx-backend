90dc9a974187b466dba384a7e98f67951d9015bb
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-28 11:44:56 +0300

    Use async/await

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,5 +1,11 @@
 import { createClient, print } from 'redis';
 
+/**
+ * A note to the task reviewer: I have used async/await
+ * because the Redis client get method being used returns a Promise.
+ * Starting from redis@4.0.0, the library has built-in support for Promises.
+ */
+
 const redisClient = createClient();
 
 redisClient.on('error', (error) => {
@@ -14,13 +20,11 @@ function setNewSchool(schoolName, value) {
   redisClient.set(schoolName, value, print);
 }
 
-function displaySchoolValue(schoolName) {
-  redisClient.get(schoolName, (_, value) => {
-    console.log(value);
-  });
+async function displaySchoolValue(schoolName) {
+  console.log(await redisClient.get(schoolName));
 }
 
 redisClient.connect();
-displaySchoolValue('Holberton');
+(async () => displaySchoolValue('Holberton'))();
 setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+(async () => displaySchoolValue('HolbertonSanFrancisco'))();
--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,12 +1,18 @@
 import { createClient, print } from 'redis';
 
+/**
+ * A note to the task reviewer: I have used async/await
+ * because the Redis client hGetAll method being used returns a Promise.
+ * Starting from redis@4.0.0, the library has built-in support for Promises.
+ */
+
 const redisClient = createClient();
 
 redisClient.on('error', (error) => {
   console.log(`Redis client not connected to the server: ${error}`);
 });
 
-redisClient.on('connect', () => {
+redisClient.on('connect', async () => {
   console.log('Redis client connected to the server');
 
   redisClient.hSet('HolbertonSchools', 'Portland', 50, print);
@@ -16,9 +22,7 @@ redisClient.on('connect', () => {
   redisClient.hSet('HolbertonSchools', 'Cali', 40, print);
   redisClient.hSet('HolbertonSchools', 'Paris', 2, print);
 
-  redisClient.hGetAll('HolbertonSchools', (_, res) => {
-    console.log(res);
-  });
+  console.log(await redisClient.hGetAll('HolbertonSchools'));
 });
 
 redisClient.connect();
